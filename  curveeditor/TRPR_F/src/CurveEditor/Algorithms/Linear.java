6d5222420c80c7363553ded3b57470a3cd661f69
staessenssibrand@hotmail.com <staessenssibrand@hotmail.com@772e54f7-f145-0410-89b0-173604b33207>
2008-05-12 18:05:47 +0000

    Zitten nu heel wat errors in (geen exceptions gecatched en zo), uiteraard... vanavond nog ewa afwerken.

---  curveeditor/TRPR_F/src/CurveEditor/Algorithms/Linear.java
+++  curveeditor/TRPR_F/src/CurveEditor/Algorithms/Linear.java
@@ -1,10 +1,15 @@
 package CurveEditor.Algorithms;
 
 import java.util.Vector;
-
 import CurveEditor.Curves.Curve;
 import CurveEditor.Curves.Point;
+import CurveEditor.Exceptions.DivisionByZeroException;
+import CurveEditor.Exceptions.InvalidArgumentException;
 
+/*
+ * Deze van Algorithm afgeleide klasse implementeert gewoon lineaire interpolatie.
+ * M.a.w., per twee punten wordt gewoon een tussenliggend lijnstuk getekend.
+ */
 public class Linear extends Algorithm {
 
 	public Linear(char type, short degree) {
@@ -19,227 +24,123 @@ public class Linear extends Algorithm {
 		super('L', (short) 1);
 	}
 
-	private int findYForX(int x, int x0, int y0, int x1, int y1) {
+	// Gegeven de coördinaten van het begin- en eindpunt én een X-waarde,
+	// zoek de bijhorende Y-waarde van het punt met die X-waarde dat op
+	// het tussenliggend lijnstuk ligt.
+	private final int findYForX(int x, int x0, int y0, int x1, int y1)
+			throws DivisionByZeroException {
+		if (x1 - x0 == 0)
+			throw new DivisionByZeroException();
+
 		return (int) ((double) y0 + (x - (double) x0)
 				* ((y1 - y0) / ((double) x1 - x0)));
 	}
 
-	private int findXForY(int y, int x0, int y0, int x1, int y1) {
+	// Gegeven de coördinaten van het begin- en eindpunt én een Y-waarde,
+	// zoek de bijhorende X-waarde van het punt met die Y-waarde dat op
+	// het tussenliggend lijnstuk ligt.
+	private final int findXForY(int y, int x0, int y0, int x1, int y1)
+			throws DivisionByZeroException {
+		if (y1 - y0 == 0)
+			throw new DivisionByZeroException();
+
 		return (int) ((double) x0 + (y - (double) y0)
 				* ((x1 - x0) / ((double) y1 - y0)));
 	}
 
-	public void calculateCurve(Curve c) {
+	// Gegeven een Vector van inputpunten, hervul de meegegeven Vector van
+	// outpunten m.b.v. het geïmplementeerde interpolatiealgoritme.
+	public final void calculate(Vector<Point> input, Vector<Point> output)
+			throws InvalidArgumentException {
+		if (input == null || output == null)
+			throw new InvalidArgumentException(
+					"Linear.java - calculate(Vector, Vector): Invalid Argument.");
+		else {
+			output.clear();
 
-		calculate( c.getInput(), c.getOutput() );
-//		new BezierSmoothing3(150).smoothCurve(c, 0.5, BezierSmoothing3.MODE.INPUT);
-	}
+			// Voor elk paar punten de interpolatiepunten berekenen.
+			for (int i = 0; i < input.size() - 1; ++i) {
 
-	public void calculate(Vector<Point> input, Vector<Point> output) {
-		output.clear();
+				// Positief verschil in x-waarden.
+				int diff1 = (input.get(i).X() < input.get(i + 1).X()) ? input
+						.get(i + 1).X()
+						- input.get(i).X() : input.get(i).X()
+						- input.get(i + 1).X();
 
-		for (int i = 0; i < input.size() - 1; ++i) {
-			int diff1 = (input.get(i).X() < input.get(i + 1).X()) ? input.get(
-					i + 1).X()
-					- input.get(i).X() : input.get(i).X()
-					- input.get(i + 1).X();
-			int diff2 = (input.get(i).Y() < input.get(i + 1).Y()) ? input.get(
-					i + 1).Y()
-					- input.get(i).Y() : input.get(i).Y()
-					- input.get(i + 1).Y();
+				// Positief verschil in y-waarden.
+				int diff2 = (input.get(i).Y() < input.get(i + 1).Y()) ? input
+						.get(i + 1).Y()
+						- input.get(i).Y() : input.get(i).Y()
+						- input.get(i + 1).Y();
 
-			if (diff1 > diff2) {
-				// eerste controlepunt ligt links van het tweede
-				if (input.get(i).X() < input.get(i + 1).X()) {
-					for (int x = input.get(i).X(); x <= input.get(i + 1).X(); ++x) {
-						output.add(new Point(x, findYForX(x, input.get(i).X(),
-								input.get(i).Y(), input.get(i + 1).X(), input
-										.get(i + 1).Y())));
+				try {
+
+					// Lijnstuk stijgt sneller volgens X dan volgens Y
+					// --> voor elke X de Y zoeken.
+					if (diff1 > diff2) {
+						// Eerste controlepunt ligt links van het tweede.
+						if (input.get(i).X() < input.get(i + 1).X()) {
+							for (int x = input.get(i).X(); x <= input
+									.get(i + 1).X(); ++x) {
+								output.add(new Point(x, findYForX(x, input.get(
+										i).X(), input.get(i).Y(), input.get(
+										i + 1).X(), input.get(i + 1).Y())));
+							}
+						}
+						// Eerste controlepunt ligt rechts van het tweede.
+						else {
+							for (int x = input.get(i).X(); x >= input
+									.get(i + 1).X(); --x) {
+								output.add(new Point(x, findYForX(x, input.get(
+										i).X(), input.get(i).Y(), input.get(
+										i + 1).X(), input.get(i + 1).Y())));
+							}
+						}
 					}
-				}
-				// eerste controlepunt ligt rechts van het tweede
-				else {
-					for (int x = input.get(i).X(); x >= input.get(i + 1).X(); --x) {
-						output.add(new Point(x, findYForX(x, input.get(i).X(),
-								input.get(i).Y(), input.get(i + 1).X(), input
-										.get(i + 1).Y())));
+					// Lijnstuk stijgt sneller volgens Y dan volgens X
+					// --> voor elke Y de X zoeken.
+					else {
+						// Eerste controlepunt ligt onder het tweede.
+						if (input.get(i).Y() < input.get(i + 1).Y()) {
+							for (int y = input.get(i).Y(); y <= input
+									.get(i + 1).Y(); ++y) {
+								output.add(new Point(findXForY(y, input.get(i)
+										.X(), input.get(i).Y(), input
+										.get(i + 1).X(), input.get(i + 1).Y()),
+										y));
+							}
+						}
+						// Eerste controlepunt ligt boven het tweede.
+						else {
+							for (int y = input.get(i).Y(); y >= input
+									.get(i + 1).Y(); --y) {
+								output.add(new Point(findXForY(y, input.get(i)
+										.X(), input.get(i).Y(), input
+										.get(i + 1).X(), input.get(i + 1).Y()),
+										y));
+							}
+						}
 					}
-				}
-			} else {
-				if (input.get(i).Y() < input.get(i + 1).Y()) {
-					for (int y = input.get(i).Y(); y <= input.get(i + 1).Y(); ++y) {
-						output.add(new Point(findXForY(y, input.get(i).X(),
-								input.get(i).Y(), input.get(i + 1).X(), input
-										.get(i + 1).Y()), y));
-					}
-				} else {
-					for (int y = input.get(i).Y(); y >= input.get(i + 1).Y(); --y) {
-						output.add(new Point(findXForY(y, input.get(i).X(),
-								input.get(i).Y(), input.get(i + 1).X(), input
-										.get(i + 1).Y()), y));
-					}
+				} catch (DivisionByZeroException e) {
+					output.clear();
+					throw new InvalidArgumentException(
+							"Linear.java - calculate(Vector, Vector): Division by zero.");
 				}
 			}
 		}
-		
 	}
 
-	@Override
-	public void calculateComplete(Curve c) {
-		calculate(c);
+	// Gegeven een Curve c, hervul zijn Vector van outputpunten vollédig met de
+	// geïnterpoleerde punten van de inputpunten.
+	public final void calculateComplete(Curve c)
+			throws InvalidArgumentException {
+		try {
+			calculate(c);
+		} catch (InvalidArgumentException e) {
+			if (c != null)
+				c.clearOutput();
+
+			throw e;
+		}
 	}
 }
-//package CurveEditor.Algorithms;
-//
-//import java.util.Vector;
-//
-//import CurveEditor.Curves.Curve;
-//import CurveEditor.Curves.Point;
-//
-////Lineair --> tussen elk paar punten een lijnstuk tekenen
-//public final class Linear extends Algorithm {
-//
-//	public Linear(char type, short degree) {
-//		super(type, (short) degree);
-//	}
-//
-//	public Linear(short degree) {
-//		super('L', (short) degree);
-//	}
-//
-//	public Linear() {
-//		super('L', (short) 1);
-//	}
-//
-//	public void calculate(Vector<Point> input, Vector<Point> output) {
-//		output.clear();
-//
-//		for (int i = 0; i < input.size() - 1; ++i) {
-//			interpolate(input.get(i), input.get(i + 1), output);
-//		}
-//	}
-//
-//	// output wordt gevuld met interpolatiepunten tussen start en end
-//	public void interpolate(Point start, Point end, Vector<Point> output) {
-//		int x0 = start.X();
-//		int y0 = start.Y();
-//		int x1 = end.X();
-//		int y1 = end.Y();
-////		boolean vertical = false;
-////
-////		double dy = 0.0, dx = 0.0, m = 0.0;
-////
-////		if (x0 < x1) {
-////			dy = y1 - y0;
-////			dx = x1 - x0;
-////			m = dy / dx;
-////		} else if (x0 > x1) {
-////			dy = y0 - y1;
-////			dx = x0 - x1;
-////			m = dy / dx;
-////		} else
-////			vertical = true;
-////
-////		// voor elke x-waarde, de y-waarde bepalen
-////		if (m >= -1 && m <= 1 && !vertical) {
-////			double y;
-////
-////			if (x0 <= x1) {
-////				y = y0;
-////				for (int x = x0; x <= x1; ++x) {
-////					output.add(new Point(x, (int) Math.floor(y + 0.5)));
-////					y += m;
-////				}
-////			} else {
-////				y = y1;
-////				for (int x = x1; x <= x0; ++x) {
-////					output.add(new Point(x, (int) Math.floor(y + 0.5)));
-////					y += m;
-////				}
-////			}
-////		}
-////
-////		// voor elke y-waarde, de x-waarde bepalen
-////		else if (!vertical) {
-////			double x, m2 = dx / dy;
-////
-////			if (y0 <= y1) {
-////				x = x0;
-////				for (int y = y0; y <= y1; ++y) {
-////					output.add((new Point((int) Math.floor(x + 0.5), y)));
-////					x += m2;
-////				}
-////			} else {
-////				x = x1;
-////				for (int y = y1; y <= y0; ++y) {
-////					output.add(new Point((int) Math.floor(x + 0.5), y));
-////					x += m2;
-////				}
-////
-////			}
-////		}
-////
-////		// verticale (m kan dan niet berekend worden)
-////		else {
-////			if (y0 <= y1) {
-////				for (int y = y0; y <= y1; ++y) {
-////					output.add(new Point(x0, y));
-////				}
-////			} else {
-////				for (int y = y1; y <= y0; ++y) {
-////					output.add(new Point(x0, y));
-////				}
-////
-////			}
-////		}
-//		
-//		boolean steep = false;
-//		if ( Math.abs( x1 - x0 ) < Math.abs( y1 - y0 ))
-//		{
-//			int hulp = x0;
-//			x0 = y0;
-//			y0 = hulp;
-//			hulp = x1;
-//			x1 = y1;
-//			y1 = hulp;
-//			steep = true;
-//		}
-//
-//		if ( x1 < x0 )
-//		{
-//			int hulp = x0;
-//			x0 = x1;
-//			x1 = hulp;
-//			hulp = y0;
-//			y0 = y1;
-//			y1 = hulp;
-//		}
-//
-//		int dx = x1 - x0;
-//		int dy = y1 - y0;
-//
-//		float m;
-//
-//		if ( dx == 0 )
-//			m = 0;
-//		else
-//			m = (float)dy / dx;
-//
-//		float y = y0;
-//		int x = x0;
-//
-//		for (; x <= x1; ++x )
-//		{
-//			if ( !steep )
-//				output.add((new Point(x, (int) Math.floor(y + 0.5))));
-//			else
-//				output.add((new Point((int) Math.floor(y + 0.5), x)));
-//			y += m;
-//		}
-//	}	
-////	}
-//
-//	public void calculateComplete(Curve c) {
-//		calculate(c);
-//	}
-//}
